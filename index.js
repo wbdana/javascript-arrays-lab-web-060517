f7e005deedc714178fedc19fcaf8f974e59cd3af
wbdana <william.b.dana@gmail.com>
2017-07-17 22:19:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  // Either of these works:
+  // return kittens.concat(name);
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  // Either of these works:
+  // return [name].concat(kittens);
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
